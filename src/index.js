621c68ba8cb97e0fbd46b6470c6a836efde39204
Duncan Kariuki <duncankariukimugo@gmail.com>
2025-03-18 11:21:58 +0300

    adds index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form=document.getElementById('create-task-form');
+  form.addEventListener('submit', function(e){e.preventDefault()
+      addtask()
+  })
+  function addtask() {
+      const text=document.getElementById('new-task-description');
+      const tasktext=text.value.trim();
+      
+      const li = document.createElement('li');
+      li.textContent=tasktext;
+      
+      const deletebutton=document.createElement('button');
+      deletebutton.textContent='❌';
+      deletebutton.addEventListener('click', function(){
+          li.remove();
+      })
+      li.appendChild(deletebutton)
+      const tasklist=document.getElementById('tasks');
+      tasklist.appendChild(li);
+      
+      text.value='';
+  }
+})
